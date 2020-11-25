e7b47b5c83a58a39b44ac8d66cf3e59ecc567a6f
Ash2oPS <ash2o@outlook.fr>
2020-11-25 17:18:13 +0100

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,8 +1,22 @@
 def initialiseGrille(grille):
-	for in range (9)
+	for i in range (0, 9):
         grille[i] = " "
 
 def afficheGrille(grille):
-    for in range (3)
+    for i in range (0, 3):
         print(grille[i])
-        print(
+        print("|")
+    -----------------------
+    for i in range (0, 3):
+        print(grille[i+3])
+        print("|")
+    -----------------------
+    for i in range (0, 3):
+        print(grille[i+6])
+        print("|")
+        
+        
+grille[9]
+
+initialiseGrille(grille)
+afficheGrille(grille) 
